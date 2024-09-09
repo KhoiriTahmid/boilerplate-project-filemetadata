d8f3341af643799ec6e6dc64838c2c23483e65d0
khoiri <muhkho2@gmail.com>
2024-09-09 11:14:05 +0700

    l

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+//import {multer} from multer
+const multer = require("multer")
 
 var app = express();
+const disk = multer.diskStorage({
+  destination:(req, file, cb)=>{cb(null,__dirname+"/data")},
+  filename:(req,file,cb)=>{cb(null,file.originalname)}
+})
+
+const upload = multer({storage:disk})
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -10,6 +18,14 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req,res)=>{
+  res.json({
+    name:req.file.filename,
+    type:req.file.mimetype,
+    size:req.file.size
+  })
+})
 
 
 
